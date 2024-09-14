5be147a9373408537f01fb208437af0049dcb561
yashvithaT <148280511+yashvithaT@users.noreply.github.com>
2024-09-13 21:32:05 -0700

    Update pltwwork.py

--- pltwwork.py
+++ pltwwork.py
@@ -1,4 +1,4 @@
-#   a114_nested_loops_4.py 
+
 import turtle as trtl
 
 painter = trtl.Turtle()
@@ -6,43 +6,135 @@ painter.penup()
 painter.goto(-200, 0)
 painter.pendown()
 
+painter.pencolor("medium orchid")
 x = -200
 y = 0
 move_x = 1
 move_y = 1
-while (x < 0):
 
-  while (y < 100):
-    x = x + move_x
-    y = y + move_y
-    painter.goto(x,y)
-  move_y = -1
-
-  while (y > 0):
-    x = x + move_x
-    y = y + move_y
-    painter.goto(x,y)
-  move_y = 1
-  
-  x = 0
-  y = 0
-  move_x = 1
-  move_y = 1
-  while (x < 200):
-
-    while (y < 100):
-      x = x + move_x
-      y = y + move_y
-      painter.goto(x,y)
+while x < 0:
+    while y < 100:
+        x += move_x
+        y += move_y
+        painter.goto(x, y)
     move_y = -1
 
-    while (y > 0):
-      x = x + move_x
-      y = y + move_y
-      painter.goto(x,y)
+    while y > 0:
+        x += move_x
+        y += move_y
+        painter.goto(x, y)
     move_y = 1
 
+    if x >= 0:
+        break  
+x = 0
+y = 0
+move_x = 1
+move_y = 1
 
+while x < 200:
+    while y < 100:
+        x += move_x
+        y += move_y
+        painter.goto(x, y)
+    move_y = -1
 
-wn = trtl.Screen()
-wn.mainloop()
+    while y > 0:
+        x += move_x
+        y += move_y
+        painter.goto(x, y)
+    move_y = 1
+
+    if x >= 200:
+        break  
+painter.penup()
+painter.goto(200, 0)  
+painter.pendown()
+
+x = 200
+y = 0
+move_x = -1
+move_y = -1
+
+while x > -100:
+    while y > -100:
+        x += move_x
+        y += move_y
+        painter.goto(x, y)
+    move_y = 1
+
+    while y < 0:
+        x += move_x
+        y += move_y
+        painter.goto(x, y)
+    move_y = -1
+
+    if x <= -100:
+        break  
+
+while True:
+    x = -200
+    y = 0
+    move_x = 1
+    move_y = 1
+
+    while x < 0:
+        while y < 100:
+            x += move_x
+            y += move_y
+            painter.goto(x, y)
+        move_y = -1
+
+        while y > 0:
+            x += move_x
+            y += move_y
+            painter.goto(x, y)
+        move_y = 1
+
+        if x >= 0:
+            break  
+    x = 0
+    y = 0
+    move_x = 1
+    move_y = 1
+
+    while x < 200:
+        while y < 100:
+            x += move_x
+            y += move_y
+            painter.goto(x, y)
+        move_y = -1
+
+        while y > 0:
+            x += move_x
+            y += move_y
+            painter.goto(x, y)
+        move_y = 1
+
+        if x >= 200:
+            break  
+    painter.penup()
+    painter.goto(200, 0)  
+    painter.pendown()
+
+    x = 200
+    y = 0
+    move_x = -1
+    move_y = -1
+
+    while x > -100:
+        while y > -100:
+            x += move_x
+            y += move_y
+            painter.goto(x, y)
+        move_y = 1
+
+        while y < 0:
+            x += move_x
+            y += move_y
+            painter.goto(x, y)
+        move_y = -1
+
+        if x <= -100:
+            break 
+
