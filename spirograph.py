b53599fe42a31f6d8a76e22462fdba2dced62852
yashvithaT <148280511+yashvithaT@users.noreply.github.com>
2024-09-13 18:08:00 -0700

    Update spirograph.py

--- spirograph.py
+++ spirograph.py
@@ -1,7 +1,26 @@
 import turtle as trtl
-turtle.penup()
-turtle.pendown()
-turtle.begin_fill()
+import random
+
+t = trtl.Turtle()
+G = 30 
+G_move = 20 
+
+t.speed(20)
 
 
+for i in range(G): 
+    current_size = G + i * G_move 
+    t.penup()
+    t.goto(-current_size / 2, current_size / 2)  
+    t.pendown()
 
+    t.color(random.choice(["medium purple", "light blue", "orchid", "violet", "medium orchid", "steel blue"]))
+    
+    for _ in range(4):  
+        t.forward(current_size)
+        t.right(90)
+        
+
+
+wn = trtl.Screen()
+wn.mainloop()
